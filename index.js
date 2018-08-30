151dc77f1558eb32cc089a06f99d3a38deb65564
@micahstubbs <micah.stubbs@gmail.com>
2018-08-30 16:46:41 -0700

    add logging to index and Network

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var util = require('./lib/util');
+var util = require('./lib/util')
 
 // Graph3d
-util.extend(exports, require('./index-graph3d'));
+util.extend(exports, require('./index-graph3d'))
 
 // Timeline & Graph2d
-util.extend(exports, require('./index-timeline-graph2d'));
+util.extend(exports, require('./index-timeline-graph2d'))
 
 // Network
-util.extend(exports, require('./index-network'));
+util.extend(exports, require('./index-network'))
+
+console.log('vis/index.js')
